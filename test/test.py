8326fe9ee25ba0e91e07213da27522d68fef7acd
Fing2525 <163541381+Fing2525@users.noreply.github.com>
2026-05-18 14:49:11 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,16 @@
 
+import cocotb
+from cocotb.triggers import Timer
+
+@cocotb.test()
+async def and_gate_test(dut):
+
+    dut.ui_in.value = 0b00000011
+    await Timer(1, units="ns")
+
+    assert dut.uo_out.value & 1 == 1
+
+    dut.ui_in.value = 0b00000010
+    await Timer(1, units="ns")
+
+    assert dut.uo_out.value & 1 == 0
